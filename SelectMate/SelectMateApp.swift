18ebacfb3c60df7f49bfba9e099a025b5787233f
Allan Amaral <allanmaralr@gmail.com>
2023-11-22 21:00:42 -0300

    Initial Commit

---
+++ SelectMate/SelectMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectMateApp.swift
+//  SelectMate
+//
+//  Created by Allan Amaral on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SelectMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
